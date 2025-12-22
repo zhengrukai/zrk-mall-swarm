c25e6db6ef6c63db09be512a6d95f37d1f1b531b
zhengrukai <1332856523@qq.com>
2025-12-22 16:04:35 +0800

    全技术应用，不包含业务代码

---
+++ mall-tiny/src/main/java/com/zrk/mall/tiny/MallTinyApplication.java
@@ -0,0 +1,15 @@
+package com.zrk.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zrk on 2025/12/22
+ */
+@SpringBootApplication
+public class MallTinyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTinyApplication.class, args);
+    }
+}
